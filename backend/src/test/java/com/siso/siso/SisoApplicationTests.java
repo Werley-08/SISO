4b5fb52d399d46cb627e9352f53c7e8269836da8
Werley <franciscowerley08@gmail.com>
2025-05-14 23:39:16 -0300

    ✨feat(backend): setup inicial com SpringBoot e Java

---
+++ backend/src/test/java/com/siso/siso/SisoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siso.siso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
