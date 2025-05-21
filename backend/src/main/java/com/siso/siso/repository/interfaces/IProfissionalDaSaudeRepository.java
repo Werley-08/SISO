d9c8eea6b710d3c736f4dedfd49f0f705133f945
Werley <franciscowerley08@gmail.com>
2025-05-21 19:07:11 -0300

    ✨feat(backend): visualiza profissionais da saude

--- backend/src/main/java/com/siso/siso/repository/interfaces/IProfissionalDaSaudeRepository.java
+++ backend/src/main/java/com/siso/siso/repository/interfaces/IProfissionalDaSaudeRepository.java
@@ -2,6 +2,9 @@ package com.siso.siso.repository.interfaces;
 
 import com.siso.siso.model.ProfissionalDaSaude;
 
+import java.util.Optional;
+
 public interface IProfissionalDaSaudeRepository{
     ProfissionalDaSaude save(ProfissionalDaSaude profissionalDaSaude);
+    Optional<ProfissionalDaSaude> findById(Integer id);
 }
