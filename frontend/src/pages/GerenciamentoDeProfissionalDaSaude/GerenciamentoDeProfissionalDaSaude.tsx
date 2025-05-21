d1a9e367691c31cdf4d663c9e1a41b7abe7fb1ac
Werley <franciscowerley08@gmail.com>
2025-05-21 15:23:40 -0300

    ✨feat(frontend): implementa search e botão de cadastro da tela de gerenciar profissional da saúde

--- frontend/src/pages/GerenciamentoDeProfissionalDaSaude/GerenciamentoDeProfissionalDaSaude.tsx
+++ frontend/src/pages/GerenciamentoDeProfissionalDaSaude/GerenciamentoDeProfissionalDaSaude.tsx
@@ -1,4 +1,6 @@
 import Sidebar from "../../components/SideBarComponents/Sidebar/Sidebar"
+import SearchBar from "../../components/SearchBar/SearchBar";
+import ActionButton from "../../components/ActionButton/ActionButton";
 import './GerenciamentoDeProfissionalDaSaude.css'
 
 const GerenciamentoDeProfissionalDaSaude = () => {
@@ -10,6 +12,21 @@ const GerenciamentoDeProfissionalDaSaude = () => {
             </div>
 
             <div className="profissional-container__content">
+
+                <div className="profissional-container__content__title">
+                    Gerenciamento de Usuários (Profissional da Saúde)
+                    <div className="profissional-container__content__title-line"></div>
+                </div>
+
+                <div className="profissional-container__content__top">
+                    <SearchBar className="searchbar-container"/>
+                    <ActionButton text="Adicionar Usuário" className="actionbutton-container"/>
+                </div>
+
+                <div className="profissional-container__content__table">
+
+                    
+                </div>
 
             </div>
         </div>
