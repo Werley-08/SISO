ed1878b88ec6a0e9db315d1ad1c8f77c43934ee6
Debora Viana <deborasviana3@gmail.com>
2025-05-22 01:58:46 -0300

    ✨feat(frontend): implementa a proteção de rotas

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -1,20 +1,42 @@
 import Login from "../pages/Login/Login";
-import DashBoard from "../pages/DashBoard/DashBoard"
+import DashBoard from "../pages/DashBoard/DashBoard";
 import GerenciamentoDeProfissionalDaSaude from "../pages/GerenciamentoDeProfissionalDaSaude/GerenciamentoDeProfissionalDaSaude";
 import GerenciamentoDeRecepcionista from "../pages/GerenciamentoDeRecepcionista/GerenciamentoDeRecepcionista";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
 
 function AppRoutes() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login />}></Route>
-                <Route path="/DashBoard" element={<DashBoard/>}></Route>
-                <Route path="/GerenciamentoDeProfissionalDaSaude" element={<GerenciamentoDeProfissionalDaSaude/>}></Route>
-                <Route path="/GerenciamentoDeRecepcionista" element={<GerenciamentoDeRecepcionista/>}></Route>
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route
+          path="/DashBoard"
+          element={
+            <ProtectedRoute>
+              <DashBoard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/GerenciamentoDeProfissionalDaSaude"
+          element={
+            <ProtectedRoute>
+              <GerenciamentoDeProfissionalDaSaude />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/GerenciamentoDeRecepcionista"
+          element={
+            <ProtectedRoute>
+              <GerenciamentoDeRecepcionista />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default AppRoutes;
