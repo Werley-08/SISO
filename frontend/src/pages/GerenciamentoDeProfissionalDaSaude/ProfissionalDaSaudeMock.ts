df15525a3a16c32f2cb29db09ab2eb2d24b201b6
Werley <franciscowerley08@gmail.com>
2025-05-23 02:52:12 -0300

    ✨feat(frontend): adiciona paginação na page de gerenciar profisisonais da saúde

--- frontend/src/pages/GerenciamentoDeProfissionalDaSaude/ProfissionalDaSaudeMock.ts
+++ frontend/src/pages/GerenciamentoDeProfissionalDaSaude/ProfissionalDaSaudeMock.ts
@@ -2,6 +2,7 @@ import type { Usuarios } from '../../types/Usuarios';
 
 export const ProfissionalDaSaudeMock: Usuarios[] = [
   {
+    id: 1,
     nome: "Dra. Ana Clara Mendes",
     username: "ana.mendes",
     senha: null,
@@ -18,6 +19,7 @@ export const ProfissionalDaSaudeMock: Usuarios[] = [
     }
   },
   {
+    id: 2,
     nome: "Dr. João Pedro Lima",
     username: "joao.p.lima",
     senha: null,
@@ -34,6 +36,7 @@ export const ProfissionalDaSaudeMock: Usuarios[] = [
     }
   },
   {
+    id: 3,
     nome: "Dra. Mariana Alves",
     username: "mariana.alves",
     senha: null,
@@ -50,6 +53,7 @@ export const ProfissionalDaSaudeMock: Usuarios[] = [
     }
   },
   {
+    id: 4,
     nome: "Dr. Carlos Eduardo",
     username: "carlos.edu",
     senha: null,
@@ -66,6 +70,7 @@ export const ProfissionalDaSaudeMock: Usuarios[] = [
     }
   },
   {
+    id: 5,
     nome: "Dra. Fernanda Souza",
     username: "fernanda.souza",
     senha: null,
@@ -82,6 +87,7 @@ export const ProfissionalDaSaudeMock: Usuarios[] = [
     }
   },
   {
+    id: 6,
     nome: "Dr. Ricardo Martins",
     username: "ricardo.martins",
     senha: null,
@@ -98,6 +104,7 @@ export const ProfissionalDaSaudeMock: Usuarios[] = [
     }
   },
   {
+    id: 7,
     nome: "Dra. Luana Ribeiro",
     username: "luana.ribeiro",
     senha: null,
@@ -114,6 +121,7 @@ export const ProfissionalDaSaudeMock: Usuarios[] = [
     }
   },
   {
+    id: 8,
     nome: "Dr. Marcelo Torres",
     username: "marcelo.torres",
     senha: null,
@@ -130,6 +138,7 @@ export const ProfissionalDaSaudeMock: Usuarios[] = [
     }
   },
   {
+    id: 9,
     nome: "Dra. Camila Ferreira",
     username: "camila.ferreira",
     senha: null,
@@ -143,6 +152,516 @@ export const ProfissionalDaSaudeMock: Usuarios[] = [
     especialidade: {
       id: 9,
       nome: "Estomatologia"
+    }
+  },
+  {
+    id: 10,
+    nome: "Dr. Rafael Oliveira",
+    username: "rafael.oliveira",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "ATIVO",
+    telefone: "(92) 98765-4321",
+    rua: "Rua das Orquídeas",
+    bairro: "Adrianópolis",
+    cidade: "Manaus",
+    numero_casa: "258",
+    especialidade: {
+      id: 10,
+      nome: "Implantodontia"
+    }
+  },
+  {
+    id: 11,
+    nome: "Dra. Juliana Santos",
+    username: "juliana.santos",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "INATIVO",
+    telefone: "(96) 91234-5678",
+    rua: "Av. dos Ipês",
+    bairro: "Centro",
+    cidade: "Macapá",
+    numero_casa: "369",
+    especialidade: {
+      id: 11,
+      nome: "Dentística"
+    }
+  },
+  {
+    id: 12,
+    nome: "Dr. Pedro Henrique",
+    username: "pedro.henrique",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "ATIVO",
+    telefone: "(69) 98765-4321",
+    rua: "Rua das Palmeiras",
+    bairro: "Centro",
+    cidade: "Porto Velho",
+    numero_casa: "741",
+    especialidade: {
+      id: 12,
+      nome: "Patologia Bucal"
+    }
+  },
+  {
+    id: 13,
+    nome: "Dra. Beatriz Costa",
+    username: "beatriz.costa",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "ATIVO",
+    telefone: "(92) 98765-4321",
+    rua: "Av. das Flores",
+    bairro: "Centro",
+    cidade: "Manaus",
+    numero_casa: "852",
+    especialidade: {
+      id: 13,
+      nome: "Odontologia Estética"
+    }
+  },
+  {
+    id: 14,
+    nome: "Dr. Lucas Mendes",
+    username: "lucas.mendes",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "INATIVO",
+    telefone: "(96) 91234-5678",
+    rua: "Rua das Acácias",
+    bairro: "Centro",
+    cidade: "Macapá",
+    numero_casa: "963",
+    especialidade: {
+      id: 14,
+      nome: "Odontologia do Trabalho"
+    }
+  },
+  {
+    id: 15,
+    nome: "Dra. Gabriela Lima",
+    username: "gabriela.lima",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "ATIVO",
+    telefone: "(92) 98765-4321",
+    rua: "Av. das Palmeiras",
+    bairro: "Centro",
+    cidade: "Manaus",
+    numero_casa: "159",
+    especialidade: {
+      id: 15,
+      nome: "Odontologia Legal"
+    }
+  },
+  {
+    id: 16,
+    nome: "Dr. Thiago Almeida",
+    username: "thiago.almeida",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "INATIVO",
+    telefone: "(98) 98765-4321",
+    rua: "Rua das Orquídeas",
+    bairro: "Centro",
+    cidade: "São Luís",
+    numero_casa: "357",
+    especialidade: {
+      id: 16,
+      nome: "Odontologia Hospitalar"
+    }
+  },
+  {
+    id: 17,
+    nome: "Dra. Carolina Silva",
+    username: "carolina.silva",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "ATIVO",
+    telefone: "(99) 98765-4321",
+    rua: "Av. das Flores",
+    bairro: "Centro",
+    cidade: "Imperatriz",
+    numero_casa: "246",
+    especialidade: {
+      id: 17,
+      nome: "Odontologia para Pacientes Especiais"
+    }
+  },
+  {
+    id: 18,
+    nome: "Dr. Bruno Santos",
+    username: "bruno.santos",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "ATIVO",
+    telefone: "(91) 98765-4321",
+    rua: "Rua das Palmeiras",
+    bairro: "Centro",
+    cidade: "Belém",
+    numero_casa: "135",
+    especialidade: {
+      id: 18,
+      nome: "Odontologia do Esporte"
+    }
+  },
+  {
+    id: 19,
+    nome: "Dra. Larissa Costa",
+    username: "larissa.costa",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "INATIVO",
+    telefone: "(93) 98765-4321",
+    rua: "Av. das Orquídeas",
+    bairro: "Centro",
+    cidade: "Santarém",
+    numero_casa: "468",
+    especialidade: {
+      id: 19,
+      nome: "Odontologia do Sono"
+    }
+  },
+  {
+    id: 20,
+    nome: "Dr. Diego Oliveira",
+    username: "diego.oliveira",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "ATIVO",
+    telefone: "(94) 98765-4321",
+    rua: "Rua das Acácias",
+    bairro: "Centro",
+    cidade: "Marabá",
+    numero_casa: "579",
+    especialidade: {
+      id: 20,
+      nome: "Odontologia Digital"
+    }
+  },
+  {
+    id: 21,
+    nome: "Dra. Mariana Lima",
+    username: "mariana.lima",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "INATIVO",
+    telefone: "(95) 98765-4321",
+    rua: "Av. das Flores",
+    bairro: "Centro",
+    cidade: "Boa Vista",
+    numero_casa: "680",
+    especialidade: {
+      id: 21,
+      nome: "Odontologia Integrada"
+    }
+  },
+  {
+    id: 22,
+    nome: "Dr. Gabriel Mendes",
+    username: "gabriel.mendes",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "ATIVO",
+    telefone: "(96) 98765-4321",
+    rua: "Rua das Palmeiras",
+    bairro: "Centro",
+    cidade: "Macapá",
+    numero_casa: "791",
+    especialidade: {
+      id: 22,
+      nome: "Odontologia Preventiva"
+    }
+  },
+  {
+    id: 23,
+    nome: "Dra. Amanda Costa",
+    username: "amanda.costa",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "ATIVO",
+    telefone: "(92) 98765-4321",
+    rua: "Av. das Orquídeas",
+    bairro: "Centro",
+    cidade: "Manaus",
+    numero_casa: "802",
+    especialidade: {
+      id: 23,
+      nome: "Odontologia Social"
+    }
+  },
+  {
+    id: 24,
+    nome: "Dr. Leonardo Silva",
+    username: "leonardo.silva",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "INATIVO",
+    telefone: "(98) 98765-4321",
+    rua: "Rua das Acácias",
+    bairro: "Centro",
+    cidade: "São Luís",
+    numero_casa: "913",
+    especialidade: {
+      id: 24,
+      nome: "Odontologia Forense"
+    }
+  },
+  {
+    id: 25,
+    nome: "Dra. Patrícia Alves",
+    username: "patricia.alves",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "ATIVO",
+    telefone: "(91) 98765-4321",
+    rua: "Av. das Flores",
+    bairro: "Centro",
+    cidade: "Belém",
+    numero_casa: "024",
+    especialidade: {
+      id: 25,
+      nome: "Odontologia Geriátrica"
+    }
+  },
+  {
+    id: 26,
+    nome: "Dr. Rodrigo Martins",
+    username: "rodrigo.martins",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "INATIVO",
+    telefone: "(93) 98765-4321",
+    rua: "Rua das Palmeiras",
+    bairro: "Centro",
+    cidade: "Santarém",
+    numero_casa: "135",
+    especialidade: {
+      id: 26,
+      nome: "Odontologia Oncológica"
+    }
+  },
+  {
+    id: 27,
+    nome: "Dra. Fernanda Oliveira",
+    username: "fernanda.oliveira",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "ATIVO",
+    telefone: "(94) 98765-4321",
+    rua: "Av. das Orquídeas",
+    bairro: "Centro",
+    cidade: "Marabá",
+    numero_casa: "246",
+    especialidade: {
+      id: 27,
+      nome: "Odontologia Neonatal"
+    }
+  },
+  {
+    id: 28,
+    nome: "Dr. André Santos",
+    username: "andre.santos",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "ATIVO",
+    telefone: "(95) 98765-4321",
+    rua: "Rua das Acácias",
+    bairro: "Centro",
+    cidade: "Boa Vista",
+    numero_casa: "357",
+    especialidade: {
+      id: 28,
+      nome: "Odontologia do Esporte"
+    }
+  },
+  {
+    id: 29,
+    nome: "Dra. Camila Mendes",
+    username: "camila.mendes",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "INATIVO",
+    telefone: "(96) 98765-4321",
+    rua: "Av. das Flores",
+    bairro: "Centro",
+    cidade: "Macapá",
+    numero_casa: "468",
+    especialidade: {
+      id: 29,
+      nome: "Odontologia do Trabalho"
+    }
+  },
+  {
+    id: 30,
+    nome: "Dr. Rafael Costa",
+    username: "rafael.costa",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "ATIVO",
+    telefone: "(92) 98765-4321",
+    rua: "Rua das Palmeiras",
+    bairro: "Centro",
+    cidade: "Manaus",
+    numero_casa: "579",
+    especialidade: {
+      id: 30,
+      nome: "Odontologia Estética"
+    }
+  },
+  {
+    id: 31,
+    nome: "Dra. Juliana Lima",
+    username: "juliana.lima",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "INATIVO",
+    telefone: "(98) 98765-4321",
+    rua: "Av. das Orquídeas",
+    bairro: "Centro",
+    cidade: "São Luís",
+    numero_casa: "680",
+    especialidade: {
+      id: 31,
+      nome: "Odontologia Legal"
+    }
+  },
+  {
+    id: 32,
+    nome: "Dr. Pedro Silva",
+    username: "pedro.silva",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "ATIVO",
+    telefone: "(99) 98765-4321",
+    rua: "Rua das Acácias",
+    bairro: "Centro",
+    cidade: "Imperatriz",
+    numero_casa: "791",
+    especialidade: {
+      id: 32,
+      nome: "Odontologia Hospitalar"
+    }
+  },
+  {
+    id: 33,
+    nome: "Dra. Beatriz Almeida",
+    username: "beatriz.almeida",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "ATIVO",
+    telefone: "(91) 98765-4321",
+    rua: "Av. das Flores",
+    bairro: "Centro",
+    cidade: "Belém",
+    numero_casa: "802",
+    especialidade: {
+      id: 33,
+      nome: "Odontologia para Pacientes Especiais"
+    }
+  },
+  {
+    id: 34,
+    nome: "Dr. Lucas Santos",
+    username: "lucas.santos",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "INATIVO",
+    telefone: "(93) 98765-4321",
+    rua: "Rua das Palmeiras",
+    bairro: "Centro",
+    cidade: "Santarém",
+    numero_casa: "913",
+    especialidade: {
+      id: 34,
+      nome: "Odontologia do Sono"
+    }
+  },
+  {
+    id: 35,
+    nome: "Dra. Gabriela Costa",
+    username: "gabriela.costa",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "ATIVO",
+    telefone: "(94) 98765-4321",
+    rua: "Av. das Orquídeas",
+    bairro: "Centro",
+    cidade: "Marabá",
+    numero_casa: "024",
+    especialidade: {
+      id: 35,
+      nome: "Odontologia Digital"
+    }
+  },
+  {
+    id: 36,
+    nome: "Dr. Thiago Lima",
+    username: "thiago.lima",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "INATIVO",
+    telefone: "(95) 98765-4321",
+    rua: "Rua das Acácias",
+    bairro: "Centro",
+    cidade: "Boa Vista",
+    numero_casa: "135",
+    especialidade: {
+      id: 36,
+      nome: "Odontologia Integrada"
+    }
+  },
+  {
+    id: 37,
+    nome: "Dra. Larissa Mendes",
+    username: "larissa.mendes",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "ATIVO",
+    telefone: "(96) 98765-4321",
+    rua: "Av. das Flores",
+    bairro: "Centro",
+    cidade: "Macapá",
+    numero_casa: "246",
+    especialidade: {
+      id: 37,
+      nome: "Odontologia Preventiva"
+    }
+  },
+  {
+    id: 38,
+    nome: "Dr. Bruno Oliveira",
+    username: "bruno.oliveira",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "ATIVO",
+    telefone: "(92) 98765-4321",
+    rua: "Rua das Palmeiras",
+    bairro: "Centro",
+    cidade: "Manaus",
+    numero_casa: "357",
+    especialidade: {
+      id: 38,
+      nome: "Odontologia Social"
+    }
+  },
+  {
+    id: 39,
+    nome: "Dra. Carolina Silva",
+    username: "carolina.silva",
+    senha: null,
+    role: "PROFISSIONAL DA SAÚDE",
+    status: "INATIVO",
+    telefone: "(98) 98765-4321",
+    rua: "Av. das Orquídeas",
+    bairro: "Centro",
+    cidade: "São Luís",
+    numero_casa: "468",
+    especialidade: {
+      id: 39,
+      nome: "Odontologia Forense"
     }
   }
 ];
