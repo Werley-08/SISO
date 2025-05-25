23e4748f441ac9aaf82f4d5be3adf6f72912f72f
Werley <franciscowerley08@gmail.com>
2025-05-25 14:18:21 -0300

    🚨test(AuthControllerTest): adiciona teste de login com sucesso

---
+++ backend/src/test/java/com/siso/siso/controller/AuthControllerTest.java
@@ -0,0 +1,66 @@
+package com.siso.siso.controller;
+
+import com.siso.siso.dto.auth.AuthDTO;
+import com.siso.siso.dto.auth.AuthResponseDTO;
+import com.siso.siso.security.configuration.SecurityConfigurationsTests;
+import com.siso.siso.security.filter.SecurityFilter;
+import com.siso.siso.service.AuthService;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.standaloneSetup;
+import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.hamcrest.Matchers.instanceOf;
+
+@WebMvcTest(AuthController.class)
+@Import(SecurityConfigurationsTests.class)
+@ActiveProfiles("test")
+public class AuthControllerTest {
+
+    @Autowired
+    private AuthController authController;
+
+    @MockBean
+    private AuthService authService;
+
+    @MockBean
+    private SecurityFilter securityFilter;
+
+    @BeforeEach
+    public void setUp() {
+        standaloneSetup(this.authController);
+    }
+
+    @Test
+    @DisplayName("Should successfully log in to the system")
+    public void logarTest1() {
+
+        AuthDTO authDTO = new AuthDTO("username", "senha");
+        AuthResponseDTO authResponseDTO = new AuthResponseDTO("ABCDEFGH");
+
+        when(this.authService.logar(any(AuthDTO.class)))
+                .thenReturn(ResponseEntity.ok(authResponseDTO));
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(authDTO)
+
+                .when()
+                .post("/api/auth/logar")
+
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("token", instanceOf(String.class));
+    }
+}
