146f9c0b4b225465bf41cd1886975ea2acc081a6
Camile <camileisi@alu.ufc.br>
2025-06-04 21:58:38 -0300

    :sparkles:feat(backend):adiciona verificacao se menor de idade esta sendo cadastrado sem responsavel

--- backend/src/main/java/com/siso/siso/service/PacienteService.java
+++ backend/src/main/java/com/siso/siso/service/PacienteService.java
@@ -8,6 +8,9 @@ import com.siso.siso.service.interfaces.IPacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 @Service
 public class PacienteService implements IPacienteService {
 
@@ -22,6 +25,13 @@ public class PacienteService implements IPacienteService {
 
     @Override
     public Paciente cadastrarPaciente(Paciente paciente) {
+        LocalDate dataAtual = LocalDate.now();
+        int idade = Period.between(paciente.getData_nascimento(), dataAtual).getYears();
+
+        if(idade < 18 && paciente.getResponsavel() == null) {
+            throw new IllegalArgumentException("Um paciente menor de idade não pode ser cadastrado sem um responsável associado!");
+        }
+
         Responsavel responsavel = paciente.getResponsavel();
         if (responsavel != null) {
             paciente.setResponsavel(responsavelRepository.save(responsavel));
