be751c78d922c332388bcadf027d6f27aa46fe93
Debora Viana <deborasviana3@gmail.com>
2025-06-10 00:11:27 -0300

    ✨feat(frontend): implementa requisição de cadastrar especialidade

--- frontend/src/services/especialidadeService.ts
+++ frontend/src/services/especialidadeService.ts
@@ -1,9 +1,17 @@
 import api from './api';
 import type { Especialidade } from '../types/Especialidade';
 
+type CadastrarEspecialidadeData = Omit<Especialidade, "id">;
+
 export const especialidadeService = {
     listarEspecialidades: async (): Promise<Especialidade[]> => {
         const response = await api.get<Especialidade[]>('/especialidade/visualizarTodos');
         return response.data;
+    },
+
+    cadastrarEspecialidade: async (data: CadastrarEspecialidadeData): Promise<Especialidade> => {
+        const response = await api.post<Especialidade>('/especialidade/cadastrar', data);
+        return response.data;
     }
+
 }; 
