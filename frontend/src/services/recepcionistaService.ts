b3bb9c74dca7eb49b8c81412f5870f0bbf6494f6
Debora Viana <deborasviana3@gmail.com>
2025-06-17 22:57:09 -0300

    ✨feat(frontend): implementa serviço para edição de recepcionistas

--- frontend/src/services/recepcionistaService.ts
+++ frontend/src/services/recepcionistaService.ts
@@ -1,7 +1,7 @@
 import api from './api';
 import type { Usuarios } from '../types/Usuarios';
 
-type CadastrarRecepcionistaData = Omit<Usuarios, 'id' | 'role' | 'status' | 'especialidade'>;
+type RecepcionistaData = Omit<Usuarios, 'id' | 'role' | 'status' | 'especialidade'>;
 
 export const recepcionistaService = {
     listarRecepcionistas: async (): Promise<Usuarios[]> => {
@@ -9,7 +9,11 @@ export const recepcionistaService = {
         return response.data;
     },
 
-    cadastrarRecepcionista: async (data: CadastrarRecepcionistaData): Promise<void> => {
+    cadastrarRecepcionista: async (data: RecepcionistaData): Promise<void> => {
         await api.post('/recepcionista/cadastrar', data);
+    },
+
+    editarRecepcionista: async (id: number, data: RecepcionistaData): Promise<void> => {
+        await api.put(`/recepcionista/editar/${id}`, data)
     }
 }; 
