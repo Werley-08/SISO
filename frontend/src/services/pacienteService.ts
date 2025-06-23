5be826ccdd915596b28c9e3cd320be748204a3a9
Debora Viana <deborasviana3@gmail.com>
2025-06-23 19:13:39 -0300

    ✨feat(frontend): implementa serviço para cadastro de pacientes

--- frontend/src/services/pacienteService.ts
+++ frontend/src/services/pacienteService.ts
@@ -1,9 +1,18 @@
+import type { Responsavel } from '@/types/Responsavel';
 import api from './api';
 import type { Paciente } from '@/types/Paciente';
+
+type CadastrarPacienteData = Omit<Paciente, 'id' | 'status' | 'Responsavel'> & {
+    Responsavel?: Omit<Responsavel, 'id'>;
+};
 
 export const pacienteService = {
     listarPacientes: async (): Promise<Paciente[]> => {
         const response = await api.get<Paciente[]>('/paciente/visualizarTodos');
         return response.data;
+    },
+
+    cadastrarPaciente: async (data: CadastrarPacienteData): Promise<void> => {
+        await api.post('/paciente/cadastrar', data);
     },
 }; 
