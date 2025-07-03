da2f186fa02668bd2a70c13d7403014fe208019c
Camile <camileisi@alu.ufc.br>
2025-07-03 13:52:52 -0300

    :sparkles:feat(backend):adiciona model de anamnese

---
+++ backend/src/main/java/com/siso/siso/model/Anamnese.java
@@ -0,0 +1,35 @@
+package com.siso.siso.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Anamnese {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private float peso;
+
+    @Column(nullable = false)
+    private float altura;
+
+    @Column(nullable = false)
+    private boolean medicamentos;
+
+    @Column(nullable = false)
+    private boolean doencasCronica;
+
+    @OneToOne
+    @JoinColumn(name = "paciente_id")
+    private Paciente id_paciente;
+}
