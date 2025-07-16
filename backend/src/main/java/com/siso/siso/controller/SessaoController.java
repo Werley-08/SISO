9445a8cda8d5720821207c7a17847be4a11f2f43
Werley <franciscowerley08@gmail.com>
2025-07-16 10:07:42 -0300

    ✨feat(backend): visualiza sessões

--- backend/src/main/java/com/siso/siso/controller/SessaoController.java
+++ backend/src/main/java/com/siso/siso/controller/SessaoController.java
@@ -1,13 +1,18 @@
 package com.siso.siso.controller;
 
 import com.siso.siso.dto.create.SessaoCreateDTO;
+import com.siso.siso.dto.response.SessaoResponseDTO;
 import com.siso.siso.dto.response.TratamentoResponseDTO;
 import com.siso.siso.service.interfaces.ISessaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 import static com.siso.siso.mapper.SessaoMapper.toModel;
 import static com.siso.siso.mapper.TratamentoMapper.toDTO;
+import static com.siso.siso.mapper.SessaoMapper.toDTO;
+import static com.siso.siso.utils.DateFormatter.formatDate;
 
 @RestController
 @RequestMapping("/api/sessao")
@@ -21,7 +26,12 @@ public class SessaoController {
     }
 
     @PostMapping("/cadastrar/{id_tratamento}")
-    public TratamentoResponseDTO cadastrarTratamento(@RequestBody SessaoCreateDTO sessaoCreateDTO, @PathVariable Integer id_tratamento) {
+    public TratamentoResponseDTO cadastrarSessao(@RequestBody SessaoCreateDTO sessaoCreateDTO, @PathVariable Integer id_tratamento) {
         return toDTO(sessaoService.cadastrarSessao(toModel(sessaoCreateDTO), id_tratamento));
+    }
+
+    @GetMapping("visualizar")
+    public List<SessaoResponseDTO> visualizarSessao(@RequestParam String data) {
+        return toDTO(sessaoService.visualizarSessao(formatDate(data)));
     }
 }
--- backend/src/main/java/com/siso/siso/service/interfaces/ISessaoService.java
+++ backend/src/main/java/com/siso/siso/service/interfaces/ISessaoService.java
@@ -3,7 +3,11 @@ package com.siso.siso.service.interfaces;
 import com.siso.siso.model.Sessao;
 import com.siso.siso.model.Tratamento;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ISessaoService {
 
     Tratamento cadastrarSessao(Sessao sessao, Integer id_tratamento);
+    List<Sessao> visualizarSessao(LocalDate data);
 }
